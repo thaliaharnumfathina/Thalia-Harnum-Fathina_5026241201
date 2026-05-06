1f4d29a4b1bc23e6fe326a634fc4cea1ed735462
thaliaharnumfathina <thaliaharnum@gmail.com>
2026-05-06 16:16:38 +0700

    pertemuan9

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use app\Http\Controllers\DosenController;
+use App\Http\Controllers\PegawaiController;
+use App\Http\Controllers\BlogController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -18,6 +20,12 @@ Route::get('blog', function () {
 Route::get('per5', function () {
     return view('pertemuan5');
 });
+
+Route::get('pert5', function () {
+    return view('pertemuan5');
+});
+
+Route::get('biodata', [DosenController::class, 'biodata']);
 
 Route::get('dosen', [DosenController::class, 'index']);
 Route::get('biodata', [DosenController::class, 'index']);
@@ -49,3 +57,10 @@ Route::get('per5', function () {
 Route::get('per5', function () {
     return view('tugaspertemuan5');
 });
+
+Route::get('/pegawai/{nama}', [PegawaiController::class, 'index']);
+Route::get('/formulir', [PegawaiController::class, 'formulir']);
+Route::post('/formulir/proses', [PegawaiController::class, 'proses']);
+
+Route::get('/blog/tentang', [BlogController::class, 'tentang']);
+Route::get('/blog/kontak', [BlogController::class, 'kontak']);
