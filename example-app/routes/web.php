d0e6cd4b2c0b7076c42f0d453e09209acd270c90
thaliaharnumfathina <thaliaharnum@gmail.com>
2026-05-11 12:16:17 +0700

    pertemuan11

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -1,66 +1,38 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use app\Http\Controllers\DosenController;
-use App\Http\Controllers\PegawaiController;
-use App\Http\Controllers\BlogController;
+use App\Http\Controllers\DosenController;
+use App\Http\Controllers\PegawaiController ;
+use App\Http\Controllers\BlogController ;
+use App\Http\Controllers\PegawaiDBController ;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('halo', function () {
-	return "<h1>Halo, Selamat datang</h1>di tutorial laravel <b>www.malasngoding.com</b>";
+	return "<h1>Halo, Selamat datang</h1> di tutorial laravel <b>www.malasngoding.com</b>";
 });
 
 Route::get('blog', function () {
-    return view('blog');
-});
-
-Route::get('per5', function () {
-    return view('pertemuan5');
+	return view('blog');
 });
 
 Route::get('pert5', function () {
-    return view('pertemuan5');
+	return view('pertemuan5');
 });
 
-Route::get('biodata', [DosenController::class, 'biodata']);
 
 Route::get('dosen', [DosenController::class, 'index']);
-Route::get('biodata', [DosenController::class, 'index']);
+Route::get('biodata', [DosenController::class, 'biodata']);
 
-Route::get('per5', function () {
-    return view('pertemuan1');
-});
-
-Route::get('per5', function () {
-    return view('pertemuan2');
-});
-
-Route::get('per5', function () {
-    return view('pertemuan3');
-});
-
-Route::get('per5', function () {
-    return view('tugaspertemuan3');
-});
-
-Route::get('per5', function () {
-    return view('pertemuan4');
-});
-
-Route::get('per5', function () {
-    return view('pertemuan5index');
-});
-
-Route::get('per5', function () {
-    return view('tugaspertemuan5');
-});
-
-Route::get('/pegawai/{nama}', [PegawaiController::class, 'index']);
+Route::get('/pegawainama/{nama}', [PegawaiController::class, 'index']);
 Route::get('/formulir', [PegawaiController::class, 'formulir']);
 Route::post('/formulir/proses', [PegawaiController::class, 'proses']);
-
+//blog
+Route::get('/blog', [BlogController::class, 'home']);
 Route::get('/blog/tentang', [BlogController::class, 'tentang']);
 Route::get('/blog/kontak', [BlogController::class, 'kontak']);
+
+//crud
+Route::get('/pegawai', [PegawaiDBController::class, 'index']);
